625de74821b08d77e07114bc81186e4199b183f7
solophone <solophone@aliyun.com>
2020-01-15 23:55:08 +0800

    首页基本布局

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,59 @@
 import React, { Component } from 'react'
+import { Layout, Menu, Icon } from 'antd'
+import { Redirect, Link } from 'react-router-dom'
+import routerList, { Iroute } from '../../config/menuConfig'
+import './Home.less'
+const { Header, Sider, Content, Footer } = Layout
+const { SubMenu, Item } = Menu
 
 export default class Home extends Component {
+  renderMenu(routes: Iroute[]) {
+    return routes.map((route) => {
+      if (route.children) {
+        return (
+          <SubMenu
+            key={route.path}
+            title={
+              <span>
+                <Icon type={route.icon} />
+                {route.name}
+              </span>
+            }
+          >
+            {this.renderMenu(route.children)}
+          </SubMenu>
+        )
+      } else {
+        return (
+          <Item key={route.path}>
+            <Link to={route.path}>
+              <Icon type={route.icon} />
+              {route.name}
+            </Link>
+          </Item>
+        )
+      }
+    })
+  }
+
   render() {
-    return <div>Home</div>
+    const token = localStorage.getItem('user_token')
+    // 如果当前用户信息不可用
+    if (!token) {
+      // 自动跳转到登陆也
+      return <Redirect to="/login" />
+    }
+    return (
+      <Layout className="main-wrapper">
+        <Sider>
+          <Menu mode="inline">{this.renderMenu(routerList)}</Menu>
+        </Sider>
+        <Layout>
+          <Header>header</Header>
+          <Content>content</Content>
+          <Footer>footer</Footer>
+        </Layout>
+      </Layout>
+    )
   }
 }
